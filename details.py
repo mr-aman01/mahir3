bd03d33eecb2259a78180eb575ca4cd3ba94f495
mr-aman01 <123452520+mr-aman01@users.noreply.github.com>
2023-02-10 23:08:15 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,4 +1,4 @@
 api_id = "8953645"
 api_hash = "66b4568bd06be74c3b92ce83dfc41299"
 bot_token = "5161083462:AAEgAId62lK30H6PsIKRHjbCzVZNpx5isjg"
-sudo_groups = -1001654849862,
+sudo_groups = -1001654849862
