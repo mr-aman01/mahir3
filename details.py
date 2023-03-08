20a9a425314fbfb755c18186c088eb335f05346b
mr-aman01 <123452520+mr-aman01@users.noreply.github.com>
2023-03-08 13:36:22 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,4 +1,4 @@
-api_id = "8953645"
-api_hash = "66b4568bd06be74c3b92ce83dfc41299"
-bot_token = "5161083462:AAEgAId62lK30H6PsIKRHjbCzVZNpx5isjg"
-sudo_groups = -1001203760350
+api_id = "8420202"
+api_hash = "0bfefaa1499a1af67985f0aafccac1ba"
+bot_token = "6112305990:AAGkuMJ1N-hMFwuuoxweuy4LsdC5CQnhsbQ"
+sudo_groups = -625595391
